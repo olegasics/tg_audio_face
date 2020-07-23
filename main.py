6716a5df0377f5a2b1cb47ffdc761e8da9a3f9d9
basicscode <asicscwcs@gmail.com>
2020-07-23 14:30:21 +0300

    change_freq successful start face rec

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ import subprocess
 import os
 import telebot
 import time
+import wave
+import cv2
 
 from telebot import apihelper
+from settings import tg_token, proxy_host, proxy_port, proxy_login, proxy_pass
 
-bot = telebot.TeleBot('1327887205:AAHwgAbPQBDkEPE5lsL-qxChN5tgPXNvXD8')
+bot = telebot.TeleBot(tg_token)
 apihelper.proxy = {
-    'https': 'http://IpwJHbsVr:fbpw6t8ys@45.153.55.193:54931'
+    'https': f'http://{proxy_login}:{proxy_pass}@{proxy_host}:{proxy_port}'
 }
 
 @bot.message_handler(commands=['start', 'help'])
@@ -20,24 +23,27 @@ def start(message):
                  'Также бот определяет, есть ли на отправленном вами фото лицо. Отправьте любую фотографию')
 
 @bot.message_handler(content_types=['voice'])
-def download_file(message):
+def download_audio(message):
     if isinstance(message, telebot.types.Message):
         file_info = bot.get_file(message.voice.file_id)
         dowloaded_file = bot.download_file(file_info.file_path)
         user_id = str(message.from_user.id)
         date = str(message.date)
+        file_name = f'static/audio_voice/{str(user_id)}-{str(date)}.wav'
         src = os.path.join('static/audio_voice', file_info.file_path.split('/')[1])
 
         with open(src, 'wb') as file:
             file.write(dowloaded_file)
         try:
-            res = convert(file_info.file_path.split('/')[1], user_id, date)
+            convert(file_info.file_path.split('/')[1], user_id, date)
+            change_freq(user_id, date)
         except:
             bot.send_message(message.from_user.id, 'Что-то пошло не так при конвертации')
         else:
             bot.send_message(message.from_user.id,'Конвертация прошла успешно')
             bot.send_document(message.from_user.id,
-                              data=open(f'static/audio_voice/{str(user_id)}-{str(date)}.wav', 'rb'))
+                              data=open(f'static/audio_voice/{str(user_id)}-{str(date)}-16kHz.wav', 'rb'))
+
 
 def convert(file_name, user_id, date):
     src_filename = f'static/audio_voice/{file_name}'
@@ -47,5 +53,32 @@ def convert(file_name, user_id, date):
     if process.returncode != 0:
         raise Exception("Something went wrong")
 
+def change_freq(user_id, date):
+    with wave.open(f'static/audio_voice/{str(user_id)}-{str(date)}.wav', 'rb') as file:
+        n_channels = file.getnchannels()
+        sample_width = file.getsampwidth()
+        framerate = 16000
+        n_frames = file.getnframes()
+        comp_type = file.getcomptype()
+        comp_name = file.getcompname()
+        print(file.getframerate())
+
+        frames = file.readframes(n_frames)
+        assert len(frames) == sample_width * n_frames
+
+    with wave.open(f'static/audio_voice/{str(user_id)}-{str(date)}-16kHz.wav', 'wb') as file:
+        params = (n_channels, sample_width, framerate, n_frames, comp_type, comp_name)
+        file.setparams(params)
+        file.writeframes(frames)
+
+@bot.message_handler(content_types=['photo'])
+def download_photo(message):
+    file_info = bot.get_file(message.photo[-1].file_id)
+    downloaded_file = bot.download_file(file_info.file_path)
+
+    src = os.path.join(f'static/images', file_info.file_path.split('/')[1])
+
+    with open(src, 'rb') as file:
+        file.write(downloaded_file)
 
 bot.polling()
---
+++ settings.py
@@ -0,0 +1,7 @@
+import os
+
+tg_token = os.getenv('tg_token')
+proxy_host = os.getenv('proxy_host')
+proxy_port = os.getenv('proxy_port')
+proxy_login = os.getenv('proxy_login')
+proxy_pass = os.getenv('proxy_pass')
